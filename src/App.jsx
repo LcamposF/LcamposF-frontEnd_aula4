c6b2b94dcda082fed3da58f888feb108dd1e7196
LcamposF <luacampod@gmail.com>
2025-05-01 15:48:34 -0300

    exercicio 4

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Exercicio2 from './assets/components/Exercicio2'
+import Campanha from './assets/components/Campanha'
+
+const App = () => {
+  return (
+    <div>
+       <Campanha mes="setembro"/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/Exercicio1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Exercicio1 = () => {
+  return (
+    <div>
+        <p> Isso é um teste!</p>
+        <img src="tec.jpg" />
+    </div>
+  )
+}
+
+export default Exercicio1
